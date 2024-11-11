e02d9219afefab514812a0160dca2887126c83ea
madhav <109799001+madhav037@users.noreply.github.com>
2024-11-11 23:10:10 +0530

    tested login signup and logout

--- client/src/app/api/admin/logout/route.ts
+++ client/src/app/api/admin/logout/route.ts
@@ -1,23 +1,19 @@
-// pages/api/logout.ts
-import { NextApiRequest, NextApiResponse } from 'next';
 import { serialize } from 'cookie';
 import { NextResponse } from 'next/server';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.setHeader('Set-Cookie', serialize('token', '', {
+export const GET = async () => {
+    const response = NextResponse.json({
+        status: 200,
+        message: 'User logged out successfully',
+    });
+
+    response.headers.set('Set-Cookie', serialize('token', '', {
         httpOnly: true,
-        // secure: process.env.NODE_ENV === 'production',
         secure: true,
-        maxAge: -1, 
+        maxAge: -1,
         path: '/',
         sameSite: 'strict',
     }));
 
-    return NextResponse.json({
-        status: 200,
-        DATA: {
-            message: 'User logged out successfully',
-        },
-        FUNCTIONNAME: 'logout',
-    })    
+    return response;
 }
