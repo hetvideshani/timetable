774993f8fe0c70c476bd4c58d95c3a5ef8ae3123
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2024-11-13 16:45:46 +0530

    resource sadi sancha

--- client/src/app/api/university/[university]/resource/route.ts
+++ client/src/app/api/university/[university]/resource/route.ts
@@ -3,13 +3,17 @@ import { NextResponse } from 'next/server';
 
 export const POST = async(req:any, res:any) => {
     const resource = await req.json();
-    const { resource_name, resource_type, capacity, duration } = resource
+    const { resource_name, type, capacity, duration } = resource
     const id = req.url!.split("university/")[1].split('/')[0]
+    console.log(resource);
+    console.log(id);
     
     try {
         const { data, error } = await supabase
             .from('resource')
-            .insert({ resource_name, resource_type, capacity, duration, uni_id:id });
+            .insert({ resource_name:resource_name, resource_type:type, capacity:capacity, duration:duration, uni_id:id });
+        
+        console.log(data);
         
             if (error) {
                 throw error
