0b2cf214c19ded88ffc29a424d2cb5c42ab788d9
Shruti Vadgama <125206895+Shrut2210@users.noreply.github.com>
2024-11-16 18:55:27 +0530

    inner_session

---
+++ client/src/app/dashboard/department/[department]/session/page.tsx
@@ -0,0 +1,238 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'next/navigation';
+import { IoClose } from "react-icons/io5";
+import { LuPencil } from "react-icons/lu";
+import { FaPlus } from "react-icons/fa";
+import { useRouter } from "next/navigation";
+
+const page = () => {
+    const [uni_id, setUni_id] = useState('');
+    const params = useParams();
+    const [sessions, setSessions] = useState([{
+        id: 0,
+        session_sequence:0,
+        do_nothing:false,
+        duration:0,
+        dept_id:0,
+      }]);
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [inputData, setInputData] = useState({
+        session_sequence:0,
+        do_nothing:false,
+        duration:0
+    });
+    const [session_id, setSession_Id] = useState(0);
+    const router = useRouter();
+    const department_id = params.department;
+
+    useEffect(() => {
+        get_uni_id();
+    }, []);
+
+    const get_uni_id = async () => {
+        let customData;
+        await fetch(window.location.href)
+          .then((res) => {
+            customData = res.headers.get("uni_id");
+            if (customData) {
+              setUni_id(customData);
+            }
+          })
+          .catch((error) => console.error("Error fetching data:", error));
+
+          await get_sessions(customData);
+      };
+
+      const handle_delete = async (session_id: any) => {
+        const response = await fetch(
+          `http://localhost:3000/api/university/${uni_id}/department/${department_id}/session/${session_id}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        if (data.status === 201) {
+          console.log("Data successfully deleted:", data);
+          setSessions(sessions.filter((data) => data.id !== session_id));
+          router.refresh();
+        } else {
+          console.error("Error deleting data:", data);
+        }
+      };
+
+      const handle_insert = () => {
+        setIsModalOpen(true);
+      };
+    
+      const handle_edit = (session_sequence:any, duration:any, do_nothing:any) => {
+        setIsModalOpen(true);
+        setInputData({session_sequence: session_sequence, duration: duration, do_nothing: do_nothing});
+      };
+
+      const handleSubmit = async (e:any) => {
+        e.preventDefault();
+        try {
+          const url =
+            session_id === 0
+              ? `http://localhost:3000/api/university/${uni_id}/department/${department_id}/session`
+              : `http://localhost:3000/api/university/${uni_id}/department/${department_id}/session/${session_id}`;
+    
+          const method = session_id === 0 ? "POST" : "PUT";
+    
+          const response = await fetch(url, {
+            method: method,
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ session_sequence: inputData.session_sequence, do_nothing: inputData.do_nothing, duration: inputData.duration}),
+          });
+          const result = await response.json();
+          console.log("Data successfully posted:", result);
+    
+          if (result.function_name === "update_session") {
+            setSessions((preSes) =>
+              preSes.map((ses) =>
+                ses.id === session_id
+                  ? { ...ses, session_sequence: inputData.session_sequence, do_nothing : inputData.do_nothing, duration: inputData.duration }
+                  : ses
+              )
+            );
+          }
+    
+          if (result.function_name === "create_session") {
+            setSessions((preSes) => [
+              ...preSes,
+              result.data[0],
+            ]);
+          }
+    
+          setIsModalOpen(false);
+          setInputData({
+            session_sequence:0,
+            do_nothing:false,
+            duration:0
+          });
+          setSession_Id(0); 
+          router.refresh();
+        } catch (error) {
+          console.error("Error posting data:", error);
+        }
+      };
+
+    const get_sessions = async (id:any) => {
+        const response = await fetch(`http://localhost:3000/api/university/${id}/department/${department_id}/session`);
+        const data = await response.json();
+        if (Array.isArray(data.data)) {
+          setSessions(data.data);
+        } else {
+          setSessions([]);
+        }
+    }
+
+    const get_session_data = sessions.map((data,index) => {
+        return (
+        <div
+        className="shadow-md hover:bg-slate-100 flex flex-col justify-center items-center w-full p-5 gap-0 font-bold rounded-sm"
+        key={index}>
+        <p className=" text-2xl text-slate-950">{data.session_sequence}</p>
+        <p className=" text-2xl text-slate-950">{data.do_nothing? "Break" : "Allocated"}</p>
+        <p className=" text-2xl text-slate-950">Duration : {data.duration}</p>
+        <div className="flex gap-1 mt-5">
+          <button
+          onClick={(e) => {
+            handle_edit(data.session_sequence, data.duration ,data.do_nothing);
+            setSession_Id(data.id);
+          }}
+            className="bg-green-600 px-3 py-1 rounded-md"
+          >
+            <LuPencil size={20} className=" text-white "></LuPencil>
+          </button>
+          <button
+            onClick={() => {
+              handle_delete(data.id);
+            }}
+            className="bg-red-600 px-3 py-1 rounded-md"
+          >
+            <IoClose size={20} className=" text-white"></IoClose>
+          </button>
+        </div>
+      </div>)
+    })
+
+    return (
+      <div className="flex flex-col gap-6 justify-center items-center p-5 w-full">
+      <div className="flex justify-between w-full">
+        <div className="text-3xl font-bold text-slate-950">Session</div>
+        <button
+          onClick={handle_insert}
+          className="flex gap-1 justify-center items-center text-xl bg-blue-600 py-1 px-3 text-white rounded-md"
+        >
+          <FaPlus></FaPlus> <div>New</div>
+        </button>
+
+        {isModalOpen && (
+          <div className="fixed top-0 left-0 right-0 bottom-0 text-slate-950 bg-gray-900 bg-opacity-50 flex justify-center items-center">
+            <div className="bg-white p-6 rounded-lg shadow-lg w-1/3">
+              <h2 className="text-lg font-bold mb-4">Add New Session</h2>
+
+              <form onSubmit={handleSubmit}>
+              <input
+                  id="data"
+                  type="number"
+                  value={inputData.session_sequence ? (inputData.session_sequence) : ""}
+                  placeholder='Enter Session Sequence'
+                  onChange={(e) => setInputData({ ...inputData, session_sequence: Number(e.target.value) })}
+                  className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+                />
+                <input
+                  id="data"
+                  type="number"
+                  value={inputData.duration ? (inputData.duration) : ""}
+                  placeholder='Enter Session Duration'
+                  onChange={(e) => setInputData({ ...inputData, duration: Number(e.target.value) })}
+                  className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+                />
+                <div className='mt-1 p-2 flex gap-3 text-gray-500 items-center w-full'>
+                <label>Is Break Session ?</label>
+                  <input
+                    id="data"
+                    type="checkbox"
+                    checked={inputData.do_nothing ? inputData.do_nothing : false}
+                    onChange={(e) => setInputData({ ...inputData, do_nothing: e.target.checked })}
+                  />
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    type="submit"
+                    className="bg-blue-600 text-white px-4 py-2 rounded-md"
+                  >
+                    Submit
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={() => setIsModalOpen(false)}
+                    className="ml-2 bg-red-600 text-white px-4 py-2 rounded-md"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="grid grid-cols-3 w-full gap-5">
+        {sessions[0].id > 0 ? get_session_data : null}
+      </div>
+    </div>
+    );
+}
+
+export default page;
--- client/src/app/dashboard/department/page.tsx
+++ client/src/app/dashboard/department/page.tsx
@@ -1,25 +1,288 @@
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import { IoClose } from "react-icons/io5";
+// import { LuPencil } from "react-icons/lu";
+// import { FaPlus } from "react-icons/fa";
+// import { useRouter } from "next/navigation";
+// import Link from "next/link";
+// import '../../department.css';
+
+// const page = () => {
+//   const [uni_id, setUni_id] = useState("");
+//   const [department_id, setDepartment_id] = useState(0);
+//   const [department, setDepartment] = useState([
+//     {
+//       id: 0,
+//       department_name: "",
+//       uni_id: 0,
+//     },
+//   ]);
+
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [inputData, setInputData] = useState("");
+//   const [isActive, setIsActive] = useState(false);
+//   const router = useRouter();
+//   useEffect(() => {
+//     get_uni_id();
+//   }, []);
+
+//   const get_uni_id = async () => {
+//     let customData;
+//     await fetch(window.location.href)
+//       .then((res) => {
+//         customData = res.headers.get("uni_id");
+//         if (customData) {
+//           setUni_id(customData);
+//         }
+//       })
+//       .catch((error) => console.error("Error fetching data:", error));
+
+//     await getDepartment(customData);
+//   };
+
+//   const getDepartment = async (id: any) => {
+//     const response = await fetch(
+//       `http://localhost:3000/api/university/${id}/department`
+//     );
+
+//     const data = await response.json();
+//     if (Array.isArray(data.data)) {
+//       setDepartment(data.data);
+//     } else {
+//       setDepartment([]);
+//     }
+//   };
+
+//   const handle_delete = async (dept_id: any) => {
+//     const response = await fetch(
+//       `http://localhost:3000/api/university/${uni_id}/department/${dept_id}`,
+//       {
+//         method: "DELETE",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+//     const data = await response.json();
+//     if (data.status === 201) {
+//       console.log("Data successfully deleted:", data);
+//       setDepartment(department.filter((dept) => dept.id !== dept_id));
+//       router.refresh();
+//     } else {
+//       console.error("Error deleting data:", data);
+//     }
+//   };
+
+//   const handle_insert = () => {
+//     setIsModalOpen(true);
+//   };
+
+//   const handle_edit = (dept_name: any) => {
+//     setIsModalOpen(true);
+//     setInputData(dept_name);
+//   };
+
+//   const handleSubmit = async (e: any) => {
+//     e.preventDefault();
+//     try {
+//       const url =
+//         department_id === 0
+//           ? `http://localhost:3000/api/university/${uni_id}/department`
+//           : `http://localhost:3000/api/university/${uni_id}/department/${department_id}`;
+
+//       const method = department_id === 0 ? "POST" : "PUT";
+
+//       const response = await fetch(url, {
+//         method: method,
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ department_name: inputData }),
+//       });
+//       const result = await response.json();
+//       console.log("Data successfully posted:", result);
+
+//       if (result.function_name === "update_department") {
+//         // Update the department in place by mapping and replacing the edited item
+//         setDepartment((prevDepartments) =>
+//           prevDepartments.map((dept) =>
+//             dept.id === department_id
+//               ? { ...dept, department_name: inputData }
+//               : dept
+//           )
+//         );
+//       }
+
+//       if (result.function_name === "create_department") {
+//         // Add the new department to the list
+//         setDepartment((prevDepartments) => [
+//           ...prevDepartments,
+//           result.data[0],
+//         ]);
+//       }
+
+//       setIsModalOpen(false);
+//       setInputData("");
+//       setDepartment_id(0); // Reset department_id for next insert
+//       router.refresh();
+//     } catch (error) {
+//       console.error("Error posting data:", error);
+//     }
+//   };
+
+//   const get_dept_data = department.map((data, index) => {
+//     return (
+//       // <div
+//       //   className="shadow-md hover:bg-slate-100 flex flex-col justify-center items-center w-full p-5 gap-0 font-bold rounded-sm"
+//       //   key={index} 
+//       // >
+//       //   <div className="main_content w-full flex justify-center items-center flex-col">
+//       //     <p className=" text-lg text-slate-900">{data.id}</p>
+//       //     <p className=" text-2xl text-slate-950">{data.department_name}</p>
+//       //     <div className="flex gap-1 mt-5">
+//       //       <button
+//       //         onClick={(e) => {
+//       //           handle_edit(data.department_name);
+//       //           setDepartment_id(data.id);
+//       //         }}
+//       //         className="bg-green-600 px-3 py-1 rounded-md"
+//       //       >
+//       //         <LuPencil size={20} className=" text-white "></LuPencil>
+//       //       </button>
+//       //       <button
+//       //         onClick={() => {
+//       //           handle_delete(data.id);
+//       //         }}
+//       //         className="bg-red-600 px-3 py-1 rounded-md"
+//       //       >
+//       //         <IoClose size={20} className=" text-white"></IoClose>
+//       //       </button>
+//       //     </div>
+//       //   </div>
+//       //   <div className="button_slide w-full h-full flex gap-2 justify-center items-center ">
+//       //     <button className="bg-slate-900 text-white font-bold py-2 px-4 rounded-lg">Branch</button>
+//       //     <button className="bg-slate-900 text-white font-bold py-2 px-4 rounded-lg">Session</button>
+//       //   </div>
+//       //   </div>
+//       <div
+//       className="shadow-md hover:bg-slate-100 flex flex-col justify-center items-center w-full p-5 gap-0 font-bold rounded-sm"
+//       key={index}
+//     >
+//       <div
+//         className={`main_content w-full flex justify-center items-center flex-col ${isActive ? "hidden" : ""}`}
+//         onClick={() => setIsActive(true)}
+//       >
+//         <p className="text-lg text-slate-900">{data.id}</p>
+//         <p className="text-2xl text-slate-950">{data.department_name}</p>
+//         <div className="flex gap-1 mt-5">
+//           <button
+//             onClick={(e) => {
+//               e.stopPropagation();
+//               handle_edit(data.department_name);
+//               setDepartment_id(data.id);
+//             }}
+//             className="bg-green-600 px-3 py-1 rounded-md"
+//           >
+//             <LuPencil size={20} className="text-white" />
+//           </button>
+//           <button
+//             onClick={(e) => {
+//               e.stopPropagation();
+//               handle_delete(data.id);
+//             }}
+//             className="bg-red-600 px-3 py-1 rounded-md"
+//           >
+//             <IoClose size={20} className="text-white" />
+//           </button>
+//         </div>
+//       </div>
+//       <div className={`button_slide w-full h-28 flex gap-2 justify-center items-center ${isActive ? "active" : ""}`} onClick={() => setIsActive(false)}>
+//         <button className="bg-slate-900 text-white font-bold py-2 px-4 rounded-lg">Branch</button>
+//         <button className="bg-slate-900 text-white font-bold py-2 px-4 rounded-lg">Session</button>
+//       </div>
+//     </div>
+//     );
+//   });
+
+//   return (
+//     <div className="flex flex-col gap-6 justify-center items-center p-5 w-full">
+//       <div className="flex justify-between w-full">
+//         <div className="text-3xl font-bold text-slate-950">Department</div>
+//         <button
+//           onClick={handle_insert}
+//           className="flex gap-1 justify-center items-center text-xl bg-blue-600 py-1 px-3 text-white rounded-md"
+//         >
+//           <FaPlus /> <div>New</div>
+//         </button>
+
+//         {isModalOpen && (
+//           <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
+//             <div className="bg-white p-6 rounded-lg shadow-lg w-1/3">
+//               <h2 className="text-lg font-bold mb-4">Add New Department</h2>
+
+//               <form onSubmit={handleSubmit}>
+//                 <input
+//                   id="data"
+//                   type="text"
+//                   value={inputData}
+//                   placeholder="Enter Department Name"
+//                   onChange={(e) => setInputData(e.target.value)}
+//                   className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+//                 />
+
+//                 <div className="mt-4">
+//                   <button
+//                     type="submit"
+//                     className="bg-blue-600 text-white px-4 py-2 rounded-md"
+//                   >
+//                     Submit
+//                   </button>
+
+//                   <button
+//                     type="button"
+//                     onClick={() => setIsModalOpen(false)}
+//                     className="ml-2 bg-red-600 text-white px-4 py-2 rounded-md"
+//                   >
+//                     Cancel
+//                   </button>
+//                 </div>
+//               </form>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//       <div className="grid grid-cols-3 w-full gap-5">
+//         {department.length > 1 ? get_dept_data : null}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default page;
+
 "use client";
 import React, { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { LuPencil } from "react-icons/lu";
 import { FaPlus } from "react-icons/fa";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
+import '../../department.css';
 
-const page = () => {
+const Page = () => {
   const [uni_id, setUni_id] = useState("");
   const [department_id, setDepartment_id] = useState(0);
   const [department, setDepartment] = useState([
-    {
-      id: 0,
-      department_name: "",
-      uni_id: 0,
-    },
-  ]);
-
+        {
+          id: 0,
+          department_name: "",
+          uni_id: 0,
+        },
+      ]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [inputData, setInputData] = useState("");
+  const [activeCard, setActiveCard] = useState<number | null>(null); // Tracks the active card
   const router = useRouter();
+
   useEffect(() => {
     get_uni_id();
   }, []);
@@ -101,7 +364,6 @@ const page = () => {
       console.log("Data successfully posted:", result);
 
       if (result.function_name === "update_department") {
-        // Update the department in place by mapping and replacing the edited item
         setDepartment((prevDepartments) =>
           prevDepartments.map((dept) =>
             dept.id === department_id
@@ -112,7 +374,6 @@ const page = () => {
       }
 
       if (result.function_name === "create_department") {
-        // Add the new department to the list
         setDepartment((prevDepartments) => [
           ...prevDepartments,
           result.data[0],
@@ -121,42 +382,67 @@ const page = () => {
 
       setIsModalOpen(false);
       setInputData("");
-      setDepartment_id(0); // Reset department_id for next insert
+      setDepartment_id(0);
       router.refresh();
     } catch (error) {
       console.error("Error posting data:", error);
     }
   };
 
-  const get_dept_data = department.map((data, index) => {
+  const get_dept_data = department.map((data: any, index: number) => {
+    const isActive = activeCard === index;
+
     return (
       <div
         className="shadow-md hover:bg-slate-100 flex flex-col justify-center items-center w-full p-5 gap-0 font-bold rounded-sm"
         key={index}
-        onClick={() => router.push(`/dashboard/department/${data.id}`)}
       >
-        <p className=" text-lg text-slate-900">{data.id}</p>
-        <p className=" text-2xl text-slate-950">{data.department_name}</p>
-        <div className="flex gap-1 mt-5">
-          <button
-            onClick={(e) => {
-              handle_edit(data.department_name);
-              setDepartment_id(data.id);
-            }}
-            className="bg-green-600 px-3 py-1 rounded-md"
-          >
-            <LuPencil size={20} className=" text-white "></LuPencil>
+        <div
+          className={`main_content w-full flex justify-center items-center flex-col ${
+            isActive ? "hidden" : ""
+          }`}
+          onClick={() => setActiveCard(index)}
+        >
+          <p className="text-lg text-slate-900">{data.id}</p>
+          <p className="text-2xl text-slate-950">{data.department_name}</p>
+          <div className="flex gap-1 mt-5">
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                handle_edit(data.department_name);
+                setDepartment_id(data.id);
+              }}
+              className="bg-green-600 px-3 py-1 rounded-md"
+            >
+              <LuPencil size={20} className="text-white" />
+            </button>
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                handle_delete(data.id);
+              }}
+              className="bg-red-600 px-3 py-1 rounded-md"
+            >
+              <IoClose size={20} className="text-white" />
+            </button>
+          </div>
+        </div>
+        <div
+          className={`button_slide w-full h-28 flex gap-2 justify-center items-center ${
+            isActive ? "active" : ""
+          }`}
+          onClick={() => setActiveCard(null)}
+        >
+          <button className="bg-slate-900 text-white font-bold py-2 px-4 rounded-lg"
+          onClick={() => router.push(`/dashboard/department/${data.id}/branch`)}>
+            Branch
           </button>
-          <button
-            onClick={() => {
-              handle_delete(data.id);
-            }}
-            className="bg-red-600 px-3 py-1 rounded-md"
-          >
-            <IoClose size={20} className=" text-white"></IoClose>
+          <button className="bg-slate-900 text-white font-bold py-2 px-4 rounded-lg"
+          onClick={() => router.push(`/dashboard/department/${data.id}/session`)}>
+            Session
           </button>
         </div>
-        </div>
+      </div>
     );
   });
 
@@ -214,4 +500,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
