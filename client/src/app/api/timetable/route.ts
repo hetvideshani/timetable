282d8c5842c3eb188761e4ae5dbf4ba59f2ee731
madhav <109799001+madhav037@users.noreply.github.com>
2024-11-21 17:13:10 +0530

    res acc

--- client/src/app/api/timetable/route.ts
+++ client/src/app/api/timetable/route.ts
@@ -1,10 +1,15 @@
 import { NextResponse } from "next/server";
 import { schedule } from "./schedule";
+import { generateAllocators, getAllocator, updateAllocator } from "@/lib/resourceAllocator";
 
 export async function POST(req: any, res: any) {
-    const body = await req.json();
+    // const body = await req.json();
     // console.log(body);
-    const response = await schedule(body)
-    return NextResponse.json({ status: 200, data: response, message: "Success" });
+    // const response = await schedule(body)
+    // await generateAllocators(29,5,8)
+    await updateAllocator(29, "Lab", 'H-404', 0,0, 30, 10)
+    const responsee = await getAllocator(29, "Lab", 'H-404')
+    console.log(responsee[0].sessions)
+    return NextResponse.json({ status: 200, data: [], message: "Success" });
 
 }
