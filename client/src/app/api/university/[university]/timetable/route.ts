4120063ea8ea2cefbd5196b9a07e186f5799dd7f
madhav <109799001+madhav037@users.noreply.github.com>
2024-12-09 16:57:59 +0530

    reset allocator done, updateFaculty done

--- client/src/app/api/university/[university]/timetable/route.ts
+++ client/src/app/api/university/[university]/timetable/route.ts
@@ -1,18 +1,14 @@
 import { NextResponse } from "next/server";
 import { schedule } from "./schedule";
-import { deleteAllAllocators, generateAllocators } from "@/lib/resourceAllocator";
+import { resetAllAllocators } from "@/lib/resourceAllocator";
 
 export async function POST(req: any, res: any) {
     try {
         const body = await req.json();
         const uni_id=req.url!.split('university/')[1].split('/')[0]; 
-        // console.log(body);  
+        console.log(body);  
         const response = await schedule(body,uni_id)
-        // await deleteAllAllocators(29, 'Faculty');
-        // await deleteAllAllocators(29, 'Auditorium');
-        // await deleteAllAllocators(29, 'Lab');
-        // await deleteAllAllocators(29, 'Lecture');
-        // await generateAllocators(29, 5, 4, true)
+        // await resetAllAllocators(uni_id, 5, 4);
 
         return NextResponse.json({ status: 200, data: response, message: "Success" });
     } catch (error: any) {
